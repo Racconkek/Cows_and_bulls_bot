9c8a0861ace84f63803adeca189641d7f16cf5b7
Raccoon_kek <Lyuda-mirosh@mail.ru>
2018-12-19 01:11:38 +0500

    wip delete useless files and create RiddlerBotAnswerHandler tests

--- src/exceptions/UserQueueException.java
+++ src/exceptions/UserQueueException.java
@@ -2,10 +2,6 @@ package exceptions;
 
 public class UserQueueException extends Exception {
 
-  public UserQueueException() {
-    super("Player queue error");
-  }
-
   public UserQueueException(String message) {
     super(message);
   }
---
+++ src/tests/tools/RiddlerBotAnswerHandlerUnitTests.java
@@ -0,0 +1,42 @@
+package tools;
+
+import com.google.inject.Guice;
+import core.BasicModule;
+import core.player.User;
+import core.primitives.HandlerAnswer;
+import core.primitives.UserGameRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tools.handler.RiddleBotAnswerHandler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RiddlerBotAnswerHandlerUnitTests {
+
+    private RiddleBotAnswerHandler handler;
+    private User user;
+
+    @BeforeEach
+    void setUp(){
+        var injector = Guice.createInjector(new BasicModule());
+        handler = injector.getInstance(RiddleBotAnswerHandler.class);
+        user = new User("TestUser1", "111", UserGameRole.WAITER);
+    }
+
+
+    @Test
+    void handleInput_WrongCountOfDigits_shouldReturnRightValue(){
+        var expected = new HandlerAnswer("Wrong count of digits", null, false);
+        var actual = handler.handleInput("111111", user);
+
+        assertEquals(expected.getFirstAnswer(), actual.getFirstAnswer());
+    }
+
+//    @Test
+//    void handleInput_WrongCountOfDigits_shouldReturnRightValue(){
+//        var expected = new HandlerAnswer("Wrong count of digits", null, false);
+//        var actual = handler.handleInput("111111", user);
+//
+//        assertEquals(expected.getFirstAnswer(), actual.getFirstAnswer());
+//    }
+}
